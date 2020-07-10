47f885d9b8a7f6ca82de853410dc12df2b6b3674
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 17:13:37 -0300

    "test(test_ocr_table.py): class of ocr table"

--- test/test_ocr_table.py
+++ test/test_ocr_table.py
@@ -6,3 +6,8 @@ class TestTable(unittest.TestCase):
     
     def test_of_tests(self):
         self.assertTrue(True)
+
+    def test_image_processing(self):
+        text = OCRTable("test/ocr.png",False)
+        type_output = isinstance(text,str)
+        self.assertTrue(type_output)
