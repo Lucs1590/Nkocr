a1175d9887981bb3e6a871db498f4da5401a3fa4
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 17:14:26 -0300

    "feat(ocr_table.py): add repr and conditions to specific types"

--- src/ocr_table/ocr_table.py
+++ src/ocr_table/ocr_table.py
@@ -9,24 +9,21 @@ from pathlib import Path
 from src.auxiliary import Auxiliary
 
 class OCRTable(object):
-    def __init__(self, image, show_performace: bool):
+    def __init__(self, image, show_performace: bool = False):
         self.aux = Auxiliary()
 
         input_type = self.aux.get_input_type(image)
         self.text = self.process_image(image, input_type)
 
+    def __repr__(self):
+        return self.text
 
     def process_image(self, image, _type):
         if _type == 1:
-            ...
+            return self.run_online_img_ocr(image)
         elif _type == 2:
-            ...
+            return self.run_path_img_ocr(image)
         elif _type == 3:
-            ...
+            return self.run_img_ocr(image)
         else:
-            ...
-        start_time = time()
-        phrase = pytesseract.image_to_string(Image.open('./images/tabela.png'), lang='por')
-        print(phrase)
-        print("Execution Time:", time() - start_time)
-        
+            raise NotImplementedError("Method to this specific processing isn't implemented yet!")
