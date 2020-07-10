f5b83a27f49a850d8e92569c3e2fd336753a2111
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 13:27:36 -0300

    "test(test): test to image type"

--- test/test_auxiliary.py
+++ test/test_auxiliary.py
@@ -1,14 +1,18 @@
 import unittest
+from PIL import Image
 from src.auxiliary import Auxiliary
 
 
+aux = Auxiliary()
 class TestAuxiliary(unittest.TestCase):
-    
+
     def test_of_tests(self):
         self.assertTrue(True)
 
     def test_image_type(self):
-        ...
+        image = Image.open("test/ocr.png")
+        input_type = aux.get_input_type(image)
+        self.assertEqual(input_type,3)
     
     def test_path_type(self):
         ...
