5601506f2e2cbc3fbfa44239bbbf5dbff67d1f96
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 15:45:57 -0300

    "feat(ocr_tables.py): completing main functions"

--- src/ocr_table/ocr_table.py
+++ src/ocr_table/ocr_table.py
@@ -10,7 +10,23 @@ from src.auxiliary import Auxiliary
 
 class OCRTable(object):
     def __init__(self, image, show_performace: bool):
-        ...
+        self.aux = Auxiliary()
+
+        input_type = self.aux.get_input_type(image)
+        self.text = self.process_image(image, input_type)
+
 
     def process_image(self, image, _type):
-        ...
+        if _type == 1:
+            ...
+        elif _type == 2:
+            ...
+        elif _type == 3:
+            ...
+        else:
+            ...
+        start_time = time()
+        phrase = pytesseract.image_to_string(Image.open('./images/tabela.png'), lang='por')
+        print(phrase)
+        print("Execution Time:", time() - start_time)
+        
