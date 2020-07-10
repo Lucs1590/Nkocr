354173ee3082070367fda801823af5a5502dd0c1
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 13:13:17 -0300

    "feat(test_auxiliary.py): add structure of new tests"

--- test/test_auxiliary.py
+++ test/test_auxiliary.py
@@ -6,3 +6,13 @@ class TestAuxiliary(unittest.TestCase):
     
     def test_of_tests(self):
         self.assertTrue(True)
+
+    def test_image_type(self):
+        ...
+    
+    def test_path_type(self):
+        ...
+    
+    def test_url_type(self):
+        ...
+    
