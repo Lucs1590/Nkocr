4d38c3a594d70dc2821519f9cf210a25c22acda2
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-10 12:40:38 -0300

    "feat(auxiliary.py): add string validation"

--- src/auxiliary/auxiliary.py
+++ src/auxiliary/auxiliary.py
@@ -15,18 +15,24 @@ class Auxiliary(object):
             raise TypeError("Invalid Input, try to send an url, path, numpy.ndarray or PIL.Image.")
     
     def is_url(self, _input):
-        regex = re.compile(
-                r'^(?:http|ftp)s?://' # http:// or https://
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-                r'localhost|' # localhost...
-                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-                r'(?::\d+)?' # optional port
-                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-        return re.match(regex, _input) is not None
+        if isinstance(_input, str):
+            regex = re.compile(
+                    r'^(?:http|ftp)s?://' # http:// or https://
+                    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                    r'localhost|' # localhost...
+                    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                    r'(?::\d+)?' # optional port
+                    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+            return re.match(regex, _input) is not None
+        else:
+            return False
 
     def is_path(self, _input):
-        file_path = path.realpath(_input)
-        return path.isfile(file_path)
+        if isinstance(_input, str):
+            file_path = path.realpath(_input)
+            return path.isfile(file_path)
+        else:
+            return False
 
     def is_image(self, _input):
         numpy_type = str(type(_input)) == "<class 'numpy.ndarray'>"
