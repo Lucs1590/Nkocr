b5846cae3f9ef166b44b64f92fb592743071661e
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-13 11:12:17 -0300

    "feat(ocr_table.py): add show_perf. into global vars"

--- src/ocr_table/ocr_table.py
+++ src/ocr_table/ocr_table.py
@@ -10,23 +10,24 @@ from src.auxiliary import Auxiliary
 
 class ocr_table(object):
     def __init__(self, image, language: str = "por", show_performace: bool = False):
-        self.define_global_vars(language)
+        self.define_global_vars(language, show_performace)
         started_time = time()
         
         input_type = self.aux.get_input_type(image)
         self.text = self.process_image(image, input_type)
         
-        execution_time = time() - started_time
+        self.execution_time = time() - started_time
 
     def __repr__(self):
-        return self.text
+        return repr(self.text) if not self.show_performace else repr([self.text, self.show_performace])
 
-    def define_global_vars(self, language):
+    def define_global_vars(self, language, show_performace):
         self.aux = Auxiliary()
-        if isinstance(language, str):
-            self.lang = language 
+        if isinstance(language, str) and isinstance(show_performace, bool):
+            self.lang = language
+            self.show_performace = show_performace
         else:
-            raise TypeError("language variable must need be a string!")
+            raise TypeError("language variable must need be a string and show_perf. bool!")
 
     def process_image(self, image, _type):
         if _type == 1:
