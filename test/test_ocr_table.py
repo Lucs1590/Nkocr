e50d69ca4d74fbb699a03277d0de3b7e08b51d45
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-13 09:40:59 -0300

    "test(test_ocr_table): making structres to each test"

--- test/test_ocr_table.py
+++ test/test_ocr_table.py
@@ -7,7 +7,13 @@ class TestTable(unittest.TestCase):
     def test_of_tests(self):
         self.assertTrue(True)
 
-    def test_image_processing(self):
+    def test_path_processing(self):
         text = OCRTable("test/ocr.png",False)
         type_output = isinstance(text,str)
         self.assertTrue(type_output)
+
+    def test_url_processing(self):
+        ...
+
+    def test_image_processing(self):
+        ...
