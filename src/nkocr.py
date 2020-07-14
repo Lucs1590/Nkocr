f5b90ef3fe4c9e5d50f164bfd15d3339e9156ff3
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-14 19:29:11 -0300

    "fix(nkocr.py): architeture of import problem"

--- src/nkocr.py
+++ src/nkocr.py
@@ -1,5 +1,7 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
 from ocr_table import ocr_table
 from auxiliary import Auxiliary
-
-if ocr_table and Auxiliary:
-    pass
