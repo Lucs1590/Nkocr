8cbc6d02c39462c0dbdd93509d8207d657b71555
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-24 10:31:03 -0300

    feat(auxiliary.py): function to make unsharp filter

--- src/auxiliary.py
+++ src/auxiliary.py
@@ -148,3 +148,16 @@ class Auxiliary(object):
         image = 255 - cv2.morphologyEx(255 - image,
                                        method, kernel, iterations=1)
         return image
+
+    def unsharp_mask(self, image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+        """Return a sharpened version of the image, using an unsharp mask."""
+        # https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm
+        blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+        sharpened = float(amount + 1) * image - float(amount) * blurred
+        sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+        sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+        sharpened = sharpened.round().astype(np.uint8)
+        if threshold > 0:
+            low_contrast_mask = np.absolute(image - blurred) < threshold
+            np.copyto(sharpened, image, where=low_contrast_mask)
+        return sharpened
