071d47abd5584b8eecbb91500a08a4fdbc4a82af
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-24 16:37:22 -0300

    build: new version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = read('README.md')
 
 setup(
     name='nkocr',
-    version='0.2.2',
+    version='1.0.0',
     description='This is a module to make specifics OCRs \
          at food products and nutricional tables.',
     url='https://github.com/Lucs1590/Nkocr',
