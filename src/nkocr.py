16067af33cd349a8b6278d89401755c0c7236bb3
Lucas Brito <lucasbsilva29@gmail.com>
2020-08-26 12:44:22 -0300

    fix(nkocr.py): wrong import

--- src/nkocr.py
+++ src/nkocr.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 import auxiliary
-from ocr_table import ocr_tableOcrTable
+from ocr_table import OcrTable
 from ocr_product import OcrProduct
 
 if OcrTable and auxiliary and OcrProduct:
