288b0e10b9dc0d0857281d98d52bff5351aa837a
Lucas Brito <lucasbsilva29@gmail.com>
2020-09-01 15:11:07 -0300

    build(tests): splitting tests in two folders
    Now we have tests of integration and unit tests into two different
    folders

--- tests/integration/test_ocr_product.py
+++ tests/integration/test_ocr_product.py
@@ -6,10 +6,13 @@ from pytest_socket import disable_socket, enable_socket
 from src.ocr_product import OcrProduct
 
 
+image_path = 'tests/ocr.png'
+
+
 class TestProduct(unittest.TestCase):
 
     def test_path_processing(self):
-        text = OcrProduct('test/ocr.png')
+        text = OcrProduct(image_path)
         type_output = isinstance(text.text, str)
         self.assertTrue(type_output)
 
@@ -26,19 +29,19 @@ class TestProduct(unittest.TestCase):
             OcrProduct('https://project-elements-nk.s3.amazonaws.com/ocr.png')
 
     def test_image_processing(self):
-        image = Image.open('test/ocr.png')
+        image = Image.open(image_path)
         text = OcrProduct(image)
         type_output = isinstance(text.text, str)
         self.assertTrue(type_output)
 
     def test_execution_time(self):
-        text_and_time = OcrProduct('test/ocr.png', show_performace=True)
+        text_and_time = OcrProduct(image_path, show_performace=True)
         has_time = text_and_time.execution_time and \
             text_and_time.show_performace and \
             len(ast.literal_eval(repr(text_and_time))) > 1
         self.assertTrue(has_time)
 
     def test_wrong_parameter_type(self):
-        image = Image.open('test/ocr.png')
+        image = Image.open(image_path)
         with self.assertRaises(TypeError):
             OcrProduct(image, True)
